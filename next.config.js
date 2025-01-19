1becfbf0bd77163ea28d489b19429db4ceace9a6
Brandon <brandonbevans@gmail.com>
2025-01-18 22:28:43 -0500

    Probably not deploy

--- next.config.js
+++ next.config.js
@@ -4,7 +4,14 @@ const nextConfig = {
   // Tells Next/Turbopack to treat these as external modules and NOT attempt
   // to bundle them.  This often fixes issues with certain node libraries
   // or server-only libraries that rely on dynamic imports.
-  serverExternalPackages: ['llamaindex']
+  serverExternalPackages: ['llamaindex'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Mark onnxruntime-node as external
+      config.externals.push('onnxruntime-node');
+    }
+    return config;
+  }
 };
 
 module.exports = nextConfig;
