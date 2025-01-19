14c452d43f2e2b31979cc544cf5d478304a925ee
Brandon <brandonbevans@gmail.com>
2025-01-19 13:55:54 -0500

    add clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -108,6 +108,17 @@ export default async function RootLayout({ children }: PropsWithChildren) {
           className="min-h-[calc(100dvh-4rem)] md:min-h[calc(100dvh-5rem)]"
         >
           {children}
+          {process.env.NODE_ENV === 'production' && (
+            <Script id="clarity-script" strategy="afterInteractive">
+              {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+            `}
+            </Script>
+          )}
         </main>
         <Footer />
         <Suspense>
