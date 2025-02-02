889fed434d4aa922508fa0c26b7567fcc3adb7c2
Brandon <brandonbevans@gmail.com>
2025-02-01 22:10:10 -0500

    revert

--- utils/postgres/postgres.ts
+++ utils/postgres/postgres.ts
@@ -20,7 +20,7 @@ const getMatchesFromEmbeddings = async (
 ): Promise<ScoredRecord[]> => {
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.SUPABASE_SERVICE_ROLE_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 
   try {
